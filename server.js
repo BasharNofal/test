beb56f4979b80098ef51d721cb0ec92600aee0df
BasharNofal <bashar.nofal1997@gmail.com>
2021-04-25 22:57:57 +0300

    .

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { ExpressPeerServer } = require("peer");
 const app = express();
 const cors  = require('cors');
+require('dotenv').config();
 app.use(cors());
 
 app.use(express.static("public"));
@@ -12,7 +13,7 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/public/index.html");
 });
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port ");
 });
 
